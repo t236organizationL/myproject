ff6a7f264945b35483c77e86f22c23c3acf67ca4
XuFeng-Luo <1723948146@qq.com>
2021-03-22 14:12:18 +0800

    '第一次提交'

---
+++ src/main/java/com/example/spring_boot_git/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_git.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/com/example/spring_boot_git/SpringBootGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_git;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootGitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
